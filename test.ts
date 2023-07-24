e15b03656ec016e2eade3f19abd5ae829550274c
Chaiyawut <chaiyawut.k@origin55.com>
2023-07-24 16:24:52 +0700

    first commit

---
+++ test.ts
@@ -0,0 +1,23 @@
+class NamedValue<T = number> {
+    private _value: T | undefined;
+  
+    constructor(private name: string) {}
+  
+    public setValue(value: T) {
+      this._value = value;
+    }
+  
+    public getValue(): T | undefined {
+      return this._value;
+    }
+  
+    public toString(): string {
+      return `${this.name}: ${this._value}`;
+    }
+}
+  
+let value = new NamedValue('myNumber');
+
+value.setValue(10);
+
+console.log(value.toString());
